4ec3a407bdba4bec93668ed221a19216d6a4c793
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-26 12:48:04 +0530

    created Home, about, contact, header, footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
       <h1>This is react router</h1>
+      <Header/>
     </>
   );
 }
